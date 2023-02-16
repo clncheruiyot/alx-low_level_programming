6ef996b6f5b3c7f37c51343b2e0f2c785f8d934a
clncheruiyot <clncheruiyot@gmail.com>
2023-02-16 12:54:05 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line with functio
+ n
+ * Return: 0 (Sucess)
+ int main(void)
+ {
+ puts("\"programming is like building a multilingual puzzle");
+ return (0);
+ }
