034fbb89fdd448a90436b52e78ad35cd557ee621
clncheruiyot <clncheruiyot@gmail.com>
2023-03-13 10:38:34 -0700

    101-main.c

--- 0x0B-malloc_free/main.c
+++ 0x0B-malloc_free/main.c
@@ -3,36 +3,49 @@
 #include <stdlib.h>
 
 /**
- * print_tab - Prints an array of string
- * @tab: The array to print
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
  *
- * Return: nothing
+ * Return: Nothing.
  */
-void print_tab(char **tab)
+void simple_print_buffer(char *buffer, unsigned int size)
 {
-    int i;
+    unsigned int i;
 
-    for (i = 0; tab[i] != NULL; ++i)
+    i = 0;
+    while (i < size)
     {
-        printf("%s\n", tab[i]);
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
     }
+    printf("\n");
 }
 
 /**
  * main - check the code for ALX School students.
  *
- * Return: 1 if an error occurred, 0 otherwise
+ * Return: Always 0.
  */
 int main(void)
 {
-    char **tab;
+    char *buffer;
 
-    tab = strtow("                ");
-    if (tab == NULL)
+    buffer = create_array(98, 'H');
+    if  (buffer == NULL)
     {
-        printf("Failed\n");
+        printf("failed to allocate memory\n");
         return (1);
     }
-    print_tab(tab);
+    simple_print_buffer(buffer, 98);
+    free(buffer);
     return (0);
 }
